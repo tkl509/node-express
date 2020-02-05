a4cec671feeb9a6d1a0eb05bd1cc97bb9d00f39f
Tami Lankford <tklankford@gmail.com>
2020-02-04 21:53:28 -0600

    Express Router

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const campsiteRouter = require('./routes/campsiteRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -9,47 +10,49 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.all('/campsites', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
+app.use('/campsites', campsiteRouter);
 
-app.get('/campsites', (req, res) => {
-    res.end('Will send all the campsites to you');
-});
+//app.all('/campsites', (req, res, next) => {
+//    res.statusCode = 200;
+//    res.setHeader('Content-Type', 'text/plain');
+//    next();
+//});
 
-app.post('/campsites', (req, res) => {
-    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
-});
+//app.get('/campsites', (req, res) => {
+//    res.end('Will send all the campsites to you');
+//});
 
-app.put('/campsites', (req, res) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /campsites');
-});
+//app.post('/campsites', (req, res) => {
+//    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+//});
 
-app.delete('/campsites', (req, res) => {
-    res.end('Deleteing all campsites');
-});
+//app.put('/campsites', (req, res) => {
+//    res.statusCode = 403;
+//    res.end('PUT operation not supported on /campsites');
+//});
 
-app.get('/campsites/:campsiteId', (req, res) => {
-    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
-});
+//app.delete('/campsites', (req, res) => {
+//    res.end('Deleteing all campsites');
+//});
 
-app.post('/campsites/:campsiteId', (req, res) => {
-    res.statusCode = 403;
-    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
-});
+//app.get('/campsites/:campsiteId', (req, res) => {
+//    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+//});
 
-app.put('/campsites/:campsiteId', (req, res) => {
-    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
-    res.end(`Will update the campsite: ${req.body.name}
-        with description: ${req.body.description}`);
-});
+//app.post('/campsites/:campsiteId', (req, res) => {
+//    res.statusCode = 403;
+//    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+//});
 
-app.delete('/campsites/:campsiteId', (req, res) => {
-    res.end(`Deleting campsite: ${req.params.campsiteId}`);
-});
+//app.put('/campsites/:campsiteId', (req, res) => {
+//    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+//    res.end(`Will update the campsite: ${req.body.name}
+//        with description: ${req.body.description}`);
+//});
+
+//app.delete('/campsites/:campsiteId', (req, res) => {
+//    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+//});
 
 app.use(express.static(__dirname + '/public'));
 
